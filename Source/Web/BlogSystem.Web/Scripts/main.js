b3a7f3a212015ea7c882ac51251a6352355a2b03
kizisoft <kizisoft@gmail.com>
2015-03-31 15:27:06 +0300

    Remove "navbar-fixed-top" Change main.js Remove "href" on votes

--- Source/Web/BlogSystem.Web/Scripts/main.js
+++ Source/Web/BlogSystem.Web/Scripts/main.js
@@ -1,11 +1,9 @@
-﻿(function () {
-    function showJsonError(data) {
-        if (data.status === 500) {
-            Notifier.notifyError('Something bad happend. Please try again!');
-            return;
-        }
-
-        var response = JSON.parse(data.responseText);
-        Notifier.notifyError(response.errorMessage);
+﻿function showJsonError(data) {
+    if (data.status === 500) {
+        Notifier.notifyError('Something bad happend. Please try again!');
+        return;
     }
-}());
+
+    var response = JSON.parse(data.responseText);
+    Notifier.notifyError(response.errorMessage);
+}
